e7544249c15f16b69b035036833ac9a5244684a6
lhb <lhb198710@gmail.com>
2016-11-10 23:19:19 +0800

    添加联系4.41和4.42

--- src/chapter4/tree_problem.c
+++ src/chapter4/tree_problem.c
@@ -17,3 +17,43 @@ void chapter4_32_problem(struct tree_node_t *node, int k1, int k2) {
         }
     }
 }
+
+static int is_tree_node_similar(struct tree_node_t *k1, struct tree_node_t *k2) {
+    if (!k1 && !k2) {
+        return 1;
+    } else if (k1 && k2) {
+        return is_tree_node_similar(k1->left, k2->left) 
+            && is_tree_node_similar(k1->right, k2->right);
+    } else {
+        return 0;
+    }
+}
+
+void chapter4_42_problem(struct tree_node_t *k1, struct tree_node_t *k2) {
+    if (is_tree_node_similar(k1, k2)) {
+        fputs("k1 and k2 is similar\n", stdout);
+    } else {
+        fputs("k1 and k2 is different\n", stdout);
+    }
+}
+
+static int is_tree_node_iosmorphic(struct tree_node_t *k1, struct tree_node_t *k2) {
+    if (!k1 && !k2) {
+        return 0;
+    } else if (k1 && k2 && k1->data == k2->data) {
+        return (is_tree_node_iosmorphic(k1->left, k2->left) && is_tree_node_iosmorphic(k1->right, k2->right))
+            || (is_tree_node_iosmorphic(k1->left, k2->right) && is_tree_node_iosmorphic(k1->right, k2->left));
+           
+    } else {
+        return 0;
+    }
+}
+
+void chapter4_43_problem(struct tree_node_t *k1, struct tree_node_t *k2) {
+    int r = is_tree_node_iosmorphic(k1, k2);
+    if (r) {
+        fputs("k1 and k2 is iosmorphic\n", stdout);
+    } else {
+        fputs("k1 and k2 is not isomorphic\n", stdout);
+    }
+}
--- src/chapter4/tree_problem.h
+++ src/chapter4/tree_problem.h
@@ -4,5 +4,7 @@
 struct tree_node_t;
 
 void chapter4_32_problem(struct tree_node_t *node, int k1, int k2);
+void chapter4_41_problem(struct tree_node_t *k1, struct tree_node_t *k2);
+void chapter4_42_problem(struct tree_node_t *k1, struct tree_node_t *k2);
 
 #endif
