3680fe6d284f6fe2956aa66bbe90fa040263706c
lhb <lhb198710@gmail.com>
2016-11-25 23:25:09 +0800

    调整编译的路径

--- build/script/gen_static_library.py
+++ build/script/gen_static_library.py
@@ -3,18 +3,21 @@
 import argparse;
 import subprocess;
 
+"""parse the runtime arguments"""
 def parse_arguments():
     parser = argparse.ArgumentParser();
     parser.add_argument("-o", type=str);
     parser.add_argument("--args", type=str);
+    parser.add_argument("--ar", type=str);
     parser.add_argument("sources", nargs="*");
     return parser.parse_args();
 
+"""The main entrance"""
 def main():
     args = parse_arguments();
     command = "rm -f %s" % (args.o);
     subprocess.check_output(command, shell=True);
-    command = "ar %s %s %s" % (args.args, args.o, " ".join(args.sources));
+    command = "%s %s %s %s" % (args.ar, args.args, args.o, " ".join(args.sources));
     subprocess.check_output(command, shell=True);
     pass;
 
