99668af969a68a87dc164e2e625098f768b73eda
lhb <lhb198710@gmail.com>
2016-12-18 21:07:47 +0800

    解决迪杰斯特拉算法代码中的bug，增加dump输出

--- src/chapter9/chapter9_3_2.c
+++ src/chapter9/chapter9_3_2.c
@@ -52,6 +52,25 @@ static struct step_node *find_step_node_from_list(struct list_head *list, int k)
     return ret;
 }
 
+static void dump_graph_path(struct graph_t *graph, int s, int k) {
+    if (s != k) {
+        dump_graph_path(graph, s, graph->queue[k].state);
+    }
+    fprintf(stdout, "%c ", graph->queue[k].name);
+}
+
+static void dump_graph_shortest_path(struct graph_t *graph, int s) {
+    struct vertex_t *ptr;
+
+    fputs("Shortest path:\n", stdout);
+    for (int i = 0; i < graph->size; ++i) {
+        ptr = graph->queue + i;
+        fprintf(stdout, "%c(%d) : ", ptr->name, ptr->distance);
+        dump_graph_path(graph, s, i);
+        fputc('\n', stdout);
+    }
+}
+
 // 获取下一步要处理的节点
 static void collect_next_list(struct graph_t *graph, int s, struct list_head *list) {
     struct vertex_t *start;
@@ -85,6 +104,7 @@ static void dijkstra(struct graph_t *graph, int s) {
     INIT_LIST_HEAD(&next_list);
     
     graph->queue[s].distance = 0;
+    graph->queue[s].state = s;
     collect_next_list(graph, s, &curr_list);
 
     while (!list_empty(&curr_list)) {
@@ -92,6 +112,9 @@ static void dijkstra(struct graph_t *graph, int s) {
         
         LIST_FOR_EACH_ENTRY(node, &curr_list, node) {
             graph->queue[node->k].distance = node->distance;
+            graph->queue[node->k].state = node->start;
+        }
+        LIST_FOR_EACH_ENTRY(node, &curr_list, node) {
             collect_next_list(graph, node->k, &next_list);
         }
         release_step_node_list(&curr_list);
@@ -108,5 +131,6 @@ void chapter9_3_2_tutorial() {
     s = graph_get_vertex_index(&graph, '1');
     DCHECK(s != -1);
     dijkstra(&graph, s);
+    dump_graph_shortest_path(&graph, s);
     free_graph(&graph);
 }
--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "chapter9/chapter9_3_1.h"
+#include "chapter9/chapter9_3_2.h"
 
 int main(int argc, char **argv) {
-	chapter9_5_b_problem();
+	chapter9_3_2_tutorial();
 	return 0;
 }
