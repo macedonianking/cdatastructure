90aca9816dd4d6f2fd6fd22ee200a79673d45cc3
lhb <lhb@oppo.com>
2017-01-02 17:48:53 +0800

    add heap operations.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "chapter6/algorithm6_2.h"
+#include "chapter6/algorithm6_5.h"
 
 int main(int argc, char **argv) {
-	algorithm6_4_tutorial();
+	algorithm6_5_1_problem();
 	return 0;
 }
