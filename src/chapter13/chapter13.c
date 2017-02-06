41bc9eb9453a225195d66df1960bd66104ae4dbf
lhb <lhb198710@gmail.com>
2017-02-06 22:19:01 +0800

    add rb-tree insert fixup.

--- src/chapter13/chapter13.c
+++ src/chapter13/chapter13.c
@@ -14,6 +14,11 @@ struct rb_node_t {
     int key;
 };
 
+struct rb_tree {
+    struct rb_node_t *root;
+    struct rb_node_t nil;
+};
+
 static inline struct rb_node_t *grantparent(struct rb_node_t *node) {
     return node->parent->parent;
 }
@@ -24,6 +29,16 @@ static inline struct rb_node_t *uncle(struct rb_node_t *node) {
     } else {
         return grantparent(node)->left;
     }
+}
+
+/**
+ * 初始化一颗红黑树
+ */
+static inline void init_rb_tree(struct rb_tree *tree) {
+    tree->root = &tree->nil;
+    tree->nil.left = tree->nil.right = tree->nil.parent = &tree->nil;
+    tree->nil.color = BLACK;
+    tree->nil.key = -1;
 }
 
 /**
@@ -42,105 +57,120 @@ static inline void free_rb_node(struct rb_node_t *node) {
     free(node);
 }
 
-static void free_rb_tree(struct rb_node_t *node) {
-    if (node != NULL) {
-        free_rb_tree(node->left);
-        free_rb_tree(node->right);
+static void free_rb_tree_internal(struct rb_tree *tree, struct rb_node_t *node) {
+    if (node != &tree->nil) {
+        free_rb_tree_internal(tree, node->left);
+        free_rb_tree_internal(tree, node->right);
         free_rb_node(node);
     }
 }
 
-/**
- * k1节点左旋
- */
-static void rb_rotate_left(struct rb_node_t **root, struct rb_node_t *k1) {
-    DCHECK(k1->right);
-    struct rb_node_t *k2;
-
-    k2 = k1->right;
-    k1->right = k2->left;
-    if (k1->right != NULL) {
-        k1->right->parent = k1;
+static void free_rb_tree(struct rb_tree *tree) {
+    if (tree->root != &tree->nil) {
+        free_rb_tree_internal(tree, tree->root);
     }
-    k2->left = k1;
-    k2->parent = k1->parent;
-    if (k1->parent == NULL) {
-        *root = k2;
-    } else if (k1->parent->left == k1){
-        k1->parent->left = k2;
-    } else {
-        k1->parent->right = k2;
-    }
-    k1->parent = k2;
+    tree->root = &tree->nil;
 }
 
 /**
- * k1节点右旋
+ * node节点下降成node右子节点的做节点->左旋
  */
-static void rb_rotate_right(struct rb_node_t **root, struct rb_node_t *k1) {
-    DCHECK(k1->left);
-    struct rb_node_t *k2;
+static void rb_rotate_left(struct rb_tree *tree, struct rb_node_t *node) {
+    struct rb_node_t *right;
 
-    k2 = k1->left;
-    k1->left = k2->right;
-    if (k1->left != NULL) {
-        k1->left->parent = k1;
+    DCHECK(node->right != &tree->nil);
+    right = node->right;
+    node->right = right->left;
+    if (node->right != &tree->nil) {
+        node->right->parent = node;
     }
-    k2->right = k1;
-    k2->parent = k1->parent;
-    if (k1->parent == NULL) {
-        *root = k2;
-    } else if (k1->parent->left == k1) {
-        k1->parent->left = k2;
+    right->parent = node->parent;
+    if (node->parent == &tree->nil) {
+        tree->root = right;
+    } else if (node->parent->left == node) {
+        node->parent->left = right;
     } else {
-        k1->parent->right = k2;
+        node->parent->right = right;
     }
-    k1->parent = k2;
+    right->left = node;
+    node->parent = right;
 }
 
-static void rb_insert_fixup(struct rb_node_t **root, struct rb_node_t *node) {
-    while (node->color == RED) {
-        if (node->parent == NULL) {
-            node->color = BLACK;
-        } else if (node->parent->color == BLACK) {
-            return;
-        } else if (uncle(node) != NULL && uncle(node)->color == RED) {
-            grantparent(node)->color = RED;
-            node->parent->color = BLACK;
-            uncle(node)->color = BLACK;
-            node = grantparent(node);
-        } else if (node->parent->right == node && grantparent(node)->left == node->parent) {
-            rb_rotate_left(root, node->parent); // node->parent左旋
-            node = node->left;
-        } else if (node->parent->left == node && grantparent(node)->right == node->parent) {
-            rb_rotate_right(root, node->parent); // node->parent右旋
-            node = node->right;
-        } else if (node->parent->left == node && grantparent(node)->left == node->parent) {
-            node->parent->color = BLACK;
-            grantparent(node)->color = RED;
-            rb_rotate_right(root, grantparent(node)); // node->parent->parent右旋
-            node = node->parent;
+/**
+ * node节点下降到左子节点的右节点位置->右旋
+ */
+static void rb_rotate_right(struct rb_tree *tree, struct rb_node_t *node) {
+    struct rb_node_t *left;
+    DCHECK(node->left);
+
+    left = node->left;
+    node->left = left->right;
+    if (node->left != &tree->nil) {
+        node->left->parent = node;
+    }
+    left->parent = node->parent;
+    if (node->parent == &tree->nil) {
+        tree->root = left;
+    } else if (node->parent->left == node) {
+        node->parent->left = left;
+    } else {
+        node->parent->right = left;
+    }
+    left->right = node;
+    node->parent = left;
+}
+
+/**
+ * 红黑树插入节点后属性修正
+ */
+static void rb_insert_fixup(struct rb_tree *tree, struct rb_node_t *node) {
+    while (node->parent->color == RED) {
+        if (node->parent == grantparent(node)->left) {
+            if (uncle(node)->color == RED) {
+                node->parent->color = BLACK;
+                uncle(node)->color = BLACK;
+                grantparent(node)->color = RED;
+                node = grantparent(node);
+            } else if (node == node->parent->right) {
+                node = node->parent;
+                rb_rotate_left(tree, node);
+            } else {
+                node->parent->color = BLACK;
+                grantparent(node)->color = RED;
+                rb_rotate_right(tree, grantparent(node));
+            }
         } else {
-            node->parent->color = BLACK;
-            grantparent(node)->color = RED;
-            rb_rotate_left(root, grantparent(node)); // node->parent->parent左旋
-            node = node->parent;
+            if (uncle(node)->color == RED) {
+                uncle(node)->color = BLACK;
+                node->parent->color = BLACK;
+                grantparent(node)->color = RED;
+                node = grantparent(node);
+            } else if (node == node->parent->left) {
+                node = node->parent;
+                rb_rotate_right(tree, node);
+            } else {
+                node->parent->color = BLACK;
+                grantparent(node)->color = RED;
+                rb_rotate_left(tree, grantparent(node));
+            }
         }
-    }   
+    }
+    tree->root->color = BLACK;
 }
 
-void rb_insert(struct rb_node_t **root, int key) {
-    struct rb_node_t *parent;
-    struct rb_node_t *node;
+/**
+ * 红黑树的插入
+ */
+static void rb_insert(struct rb_tree *tree, int key) {
+    struct rb_node_t *p, *node;
+    p = &tree->nil;
+    node = tree->root;
 
-    parent = NULL;
-    node = *root;
-    while (node != NULL) {
-        if (node->key == key) {
+    while (node != &tree->nil) {
+        p = node;
+        if (key == node->key) {
             return;
-        }
-        parent = node;
-        if (key < node->key) {
+        } else if (key < node->key) {
             node = node->left;
         } else {
             node = node->right;
@@ -148,16 +178,17 @@ void rb_insert(struct rb_node_t **root, int key) {
     }
 
     node = alloc_rb_node(key);
-    if (parent == NULL) {
-        *root = node;
-    } else if (key < parent->key) {
-        parent->left = node;
+    node->parent = p;
+    if (p == &tree->nil) {
+        tree->root = node;
+    } else if (key < p->key) {
+        p->left = node;
     } else {
-        parent->right = node;
+        p->right = node;
     }
+    node->left = node->right = &tree->nil;
     node->color = RED;
-    node->parent = parent;
-    rb_insert_fixup(root, node);
+    rb_insert_fixup(tree, node);
 }
 
 void chapter13_main() {
@@ -166,13 +197,12 @@ void chapter13_main() {
 
 void chapter13_3_2_problem() {
     int buf[] = {41, 38, 31, 12, 19, 8};
-    struct rb_node_t *root;
+    struct rb_tree tree;
 
-    root = NULL;
+    init_rb_tree(&tree);
     for (int i = 0; i < NARRAY(buf); ++i) {
-        rb_insert(&root, buf[i]);
+        rb_insert(&tree, buf[i]);
     }
 
-    free_rb_tree(root);
-    root = NULL;
+    free_rb_tree(&tree);
 }
