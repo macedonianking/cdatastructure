6850c4538e8b348d30e6e69dca61b9cdf64b64bd
lhb <lhb@oppo.com>
2017-02-13 20:49:25 +0800

    add exercise.

--- src/chapter14/chapter14_2.c
+++ src/chapter14/chapter14_2.c
@@ -1,5 +1,8 @@
 #include "chapter14/chapter14_2.h"
 
+#include "util.h"
+#include "macros.h"
+
 struct rb_int_node {
     struct rb_int_node *left, *right, *p;
     int low, high, max;
@@ -33,13 +36,61 @@ rb_int_tree_search(struct rb_int_tree *tree, int low, int high) {
 
     node = tree->root;
     while (node != &tree->nil && int_overlap(node, low, high)) {
-        if (node->left != &tree->nil && low < node->left->max) {
+        if (node->left != &tree->nil && low <= node->left->max) {
             node = node->left;
         } else {
             node = node->right;
         }
     }
     return node;
+}
+
+static inline void rb_int_left_rotate(struct rb_int_tree *tree,
+                                      struct rb_int_node *node) {
+    struct rb_int_node *right;
+
+    right = node->right;
+    DCHECK(right != &tree->nil);
+    node->right = right->left;
+    if (node->right != &tree->nil) {
+        node->right->p = node;
+    }
+    right->p = node->p;
+    if (node->p == &tree->nil) {
+        tree->root  = right;
+    } else if (node == node->p->left) {
+        node->p->left = right;
+    } else {
+        node->p->right = right;
+    }
+    right->left = node;
+    node->p = right;
+    right->max = node->max;
+    node->max = MAX(node->high, MAX(node->left->max, node->right->max));
+}
+
+static inline void rb_int_right_rotate(struct rb_int_tree *tree,
+                                       struct rb_int_node *node) {
+    struct rb_int_node *left;
+
+    left = node->left;
+    DCHECK(left != &tree->nil);
+    node->left = left->right;
+    if (node->left != &tree->nil) {
+        node->left->p = node;
+    }
+    left->p = node->p;
+    if (node->p == &tree->nil) {
+        tree->root = left;
+    } else if (node->p->left == node) {
+        node->p->left = left;
+    } else {
+        node->p->right = left;
+    }
+    left->right = node;
+    node->p = left;
+    left->max = node->max;
+    node->max = MAX(node->high, MAX(node->left->max, node->right->max));
 }
 
 void chapter14_2_main() {
