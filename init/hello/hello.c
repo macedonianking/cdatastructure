10799dc87dd1b9b7360e40aeafbb363e8666a6f6
lhb <lhb@oppo.com>
2017-03-17 20:51:28 +0800

    添加一个脚本的测试工程

---
+++ init/hello/hello.c
@@ -0,0 +1,8 @@
+#include "hello.h"
+
+#include <stdio.h>
+
+void print_two_hello() {
+    printf("Hello world!!!\n");
+    printf("Hello world!!!\n");
+}
---
+++ init/hello/hello.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_COMMON_H
+#define HELLO_COMMON_H
+
+void __declspec(dllexport) print_two_hello();
+
+#endif 
