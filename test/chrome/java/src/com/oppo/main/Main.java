e09243b3d510a4d84c99b9ffab4b7577673629e4
lhb <lhb198710@gmail.com>
2017-05-20 19:27:06 +0800

    解决一部分内容的编译问题

---
+++ test/chrome/java/src/com/oppo/main/Main.java
@@ -0,0 +1,15 @@
+package com.oppo.main;
+
+/**
+ * @author lhb
+ * @date 2017年4月16日
+ */
+public class Main {
+
+	public Main() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!!!");
+	}
+}
