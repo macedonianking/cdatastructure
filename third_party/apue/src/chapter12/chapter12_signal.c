8d1ed78669e223e619dcc0ee652dd910de312dff
mikie <lhb198710@gmail.com>
2017-07-03 23:41:20 +0800

    add pthread_sigmask, pthread_kill

--- third_party/apue/src/chapter12/chapter12_signal.c
+++ third_party/apue/src/chapter12/chapter12_signal.c
@@ -66,16 +66,55 @@ static void chapter12_8_1_post_timer(struct chapter12_8_1_params *params, long m
     pthread_mutex_unlock(&params->p_lock);
 }
 
-static void chapter12_8_1_check_expired(struct chapter12_8_1_params *params) {
+static int chapter12_8_1_check_expired(struct chapter12_8_1_params *params) {
+    long millis;
+    struct chapter12_8_1_message *ptr;
+    struct itimerval val;
+    int r;
 
+    r = 0;
+    millis = current_time_millis();
+    pthread_mutex_lock(&params->p_lock);
+    LIST_FOR_EACH_ENTRY_SAFE(ptr, &params->p_wait_list, m_node) {
+        if (ptr->m_time > millis) {
+            break;
+        }
+        list_del(&ptr->m_node);
+        list_add_tail(&ptr->m_node, &params->p_done_list);
+    }
+    if (!list_empty(&params->p_wait_list)) {
+        ptr = list_entry(params->p_wait_list.next, struct chapter12_8_1_message, m_node);
+        millis = ptr->m_time - millis;
+        if (millis < 1) {
+            millis = 1;
+        }
+        millis_to_timeval(0, &val.it_interval);
+        millis_to_timeval(millis, &val.it_value);
+        if (setitimer(ITIMER_REAL, &val, NULL)) {
+            LOGE("setitimer FATAL");
+            exit(-1);
+        }
+    } else {
+        params->p_is_finish = 1;
+        r = 1;
+    }
+    pthread_cond_signal(&params->p_cond);
+    pthread_mutex_unlock(&params->p_lock);
+    return r;
 }
 
 static void* chapter12_8_1_thread_func(void *args) {
-    sigset_t mask;
+    sigset_t mask, old_mask;
     int signo;
+    struct chapter12_8_1_params *params;
 
+    params = (struct chapter12_8_1_params*) args;
     sigemptyset(&mask);
     sigaddset(&mask, SIGALRM);
+    if (pthread_sigmask(SIG_BLOCK, &mask, &old_mask)) {
+        LOGE("pthread_sigmask FATAL");
+        exit(-1);
+    }
 
     for (;;) {
         if (sigwait(&mask, &signo)) {
@@ -86,16 +125,44 @@ static void* chapter12_8_1_thread_func(void *args) {
                 exit(-1);
             }
         } else {
-
+            if (chapter12_8_1_check_expired(params) == 1) {
+                break;
+            }
         }
+    }
+
+    if (pthread_sigmask(SIG_SETMASK, &old_mask, NULL)) {
+        LOGE("pthread_sigmask FATAL");
+        exit(-1);
     }
     return NULL;
 }
 
 static void chapter12_8_1_handler(void *args) {
+    LOGD("chapter12_8_1_handler");
 }
 
 static void chapter12_8_1_check_done_list_locked(struct chapter12_8_1_params *params) {
+    struct chapter12_8_1_message *msg;
+
+    for (;;) {
+        msg = NULL;
+        if (list_empty(&params->p_done_list)) {
+            break;
+        }
+        msg = list_entry(params->p_done_list.next, struct chapter12_8_1_message, m_node);
+        list_del(&msg->m_node);
+
+        if (pthread_mutex_unlock(&params->p_lock)) {
+            LOGE("pthread_mutex_unlock FATAL");
+        }
+        msg->m_handler(msg->m_data);
+        free(msg);
+        if (pthread_mutex_lock(&params->p_lock)) {
+            LOGE("pthread_mutex_lock FATAL");
+            exit(-1);
+        }
+    }
 }
 
 void chapter12_8_1(int argc, char **argv) {
