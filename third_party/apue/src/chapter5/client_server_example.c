56b6599382bce6660d35423139272deb3e4eb4c1
lhb <lhb@oppo.com>
2017-07-19 18:56:47 +0800

    add server.

--- third_party/apue/src/chapter5/client_server_example.c
+++ third_party/apue/src/chapter5/client_server_example.c
@@ -1,20 +1,25 @@
 #include "chapter5/client_server_example.h"
 
+#include <limits.h>
+
 #define LOG_TAG "client_server_example"
 #include "utils/net.h"
 #include "utils/log.h"
 #include "utils/apue.h"
+
+/**
+ * The server read from client and write to stdout and write back to client fd.
+ */
+static int server_echo_client(int fd);
 
 void client_server_example_main(int argc, char **argv) {
     client_server_example_main_1(argc, argv);
 }
 
 static void client_server_example_main_1_client(int argc, char **argv) {
-
 }
 
 static void server_process_service_client(int fd, int client_fd, struct sockaddr_in *addr, socklen_t len) {
-
 }
 
 /**
@@ -58,4 +63,81 @@ void client_server_example_main_1(int argc, char **argv) {
         &client_server_example_main_1_client);
 }
 
+static int server_echo_read(int fd, char *buf, int size, int *out_count) {
+    struct pollfd wait;
+    int count, remain, is_newline;
+    int n;
+    int r;
 
+    count = 0;
+    remain = size - 1;
+    memset(&wait, 0, sizeof(wait));
+    wait.fd = fd;
+    wait.events = POLLIN | POLLPRI;
+
+    r = 0;
+    while (remain > 0) {
+        if (poll(&wait, 1, -1) == -1) {
+            if (errno == EINTR) {
+                continue;
+            }
+            r = -1;
+            break;
+        } else if (wait.revents & (POLLIN | POLLPRI)) {
+            if ((n = read(fd, buf + count, remain)) == -1) {
+                if (errno == EINTR) {
+                    continue;
+                }
+                r = -1;
+                break;
+            } else if (!n) {
+                break;
+            } else {
+                buf[count + n] = '\n';
+                is_newline = strchr(buf + count, '\n') != NULL;
+                count += n;
+                remain -= n;
+                if (is_newline || !remain) {
+                    break;
+                }
+            }
+        } else if (wait.revents & (POLLERR | POLLHUP | POLLINVAL)) {
+            r = -1;
+            break;
+        }
+    }
+
+    // There is data, make the call successful.
+    if (r && count) {
+        r = 0;
+    }
+    return r;
+}
+
+static int server_echo_write(int fd, char *buf, int count) {
+    return 0;
+}
+
+int server_echo_client(int fd) {
+    char buf[LINE_MAX];
+    int count;
+    int n;
+
+    r = 0;
+    apue_set_fl(fd, O_NONBLOCK);
+    for (;;) {
+        if (server_echo_read(fd, buf, LINE_MAX, &count)) {
+            r = -1;
+            break;
+        }
+        if (!count) {
+            break;
+        }
+        if (server_echo_write(fd, buf, count)) {
+            r = -1;
+            break;
+        }
+    }
+
+    return r;
+}
